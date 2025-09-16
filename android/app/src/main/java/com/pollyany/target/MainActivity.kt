59816937193f77431241f3e774e8c76e59de3a4f
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-15 23:31:54 -0300

    componente de loading

--- android/app/src/main/java/com/pollyany/target/MainActivity.kt
+++ android/app/src/main/java/com/pollyany/target/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.target
+package com.pollyany.target
 
 import android.os.Build
 import android.os.Bundle
---
+++ src/app/index.tsx
@@ -0,0 +1,19 @@
+import { router } from "expo-router";
+import { Button, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center" }}>
+      <Text>Expo Router</Text>
+      <Button title="Nova meta" onPress={() => router.navigate("/target")} />
+      <Button
+        title="Transação"
+        onPress={() => router.navigate("/transaction/765890")}
+      />
+      <Button
+        title="Progresso"
+        onPress={() => router.navigate("/in-progress/12")}
+      />
+    </View>
+  );
+}
