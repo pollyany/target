547babc34342564d79d1bd6c805ac6b4f53b9dbb
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-17 22:51:51 -0300

    Componente Summary

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -3,14 +3,15 @@ import { styles } from "./styles";
 import { colors } from "@/theme/colors";
 import { Text, View } from "react-native";
 import { Separator } from "../Separator";
+import { Summary } from "../Summary";
 
 export type HomeHeaderProps = {
-  total: string
-}
+  total: string;
+};
 
 type Props = {
-  data: HomeHeaderProps
-}
+  data: HomeHeaderProps;
+};
 
 export function HomeHeader({ data }: Props) {
   return (
@@ -19,9 +20,22 @@ export function HomeHeader({ data }: Props) {
       style={styles.container}
     >
       <View>
-        <Text style={styles.label}>Total que você possui</Text><Text style={styles.total}>{data.total}</Text>
+        <Text style={styles.label}>Total que você possui</Text>
+        <Text style={styles.total}>{data.total}</Text>
       </View>
       <Separator color={colors.blue[400]} />
+      <View style={styles.summary}>
+        <Summary
+          data={{ label: "Entradas", value: "R$ 6.184,90" }}
+          icon={{ name: "arrow-upward", color: colors.green[500] }}
+        />
+
+        <Summary
+          isLeft
+          data={{ label: "Saídas", value: "-R$ 883,65" }}
+          icon={{ name: "arrow-downward", color: colors.red[400] }}
+        />
+      </View>
     </LinearGradient>
   );
 }
