b390839ea0f689e62bdaa2126c70c5a6826f70ba
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-17 23:09:38 -0300

    Componente Button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,8 @@
+import { Button } from "@/components/Button";
 import { HomeHeader } from "@/components/HomeHeader";
 import { List } from "@/components/List";
 import { Target } from "@/components/Target";
+import { router } from "expo-router";
 import { View } from "react-native";
 
 const summary = {
@@ -11,27 +13,27 @@ const summary = {
 
 const targets = [
   {
-    id: '1',
-    name: 'Apple Watch',
-    percentage: '50%',
-    current: 'R$ 580,00',
-    target: 'R$ 1.790,00',
+    id: "1",
+    name: "Apple Watch",
+    percentage: "50%",
+    current: "R$ 580,00",
+    target: "R$ 1.790,00",
   },
   {
-    id: '2',
-    name: 'Comprar uma cadeira ergonômica',
-    percentage: '75%',
-    current: 'R$ 900,00',
-    target: 'R$ 1.200,00',
+    id: "2",
+    name: "Comprar uma cadeira ergonômica",
+    percentage: "75%",
+    current: "R$ 900,00",
+    target: "R$ 1.200,00",
   },
   {
-    id: '3',
-    name: 'Comprar uma cadeira ergonômica',
-    percentage: '75%',
-    current: 'R$ 1.200,00',
-    target: 'R$ 3.000,00',
+    id: "3",
+    name: "Comprar uma cadeira ergonômica",
+    percentage: "75%",
+    current: "R$ 1.200,00",
+    target: "R$ 3.000,00",
   },
-]
+];
 
 export default function Index() {
   return (
@@ -39,12 +41,20 @@ export default function Index() {
       <HomeHeader data={summary} />
       <List
         title="Metas"
-        data={[]}
+        data={targets}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Target data={item} />}
+        renderItem={({ item }) => (
+          <Target
+            data={item}
+            onPress={() => router.navigate(`/in-progress/${item.id}`)}
+          />
+        )}
         emptyMessage="Nenhuma meta. Toque em nova meta para criar."
         containerStyle={{ paddingHorizontal: 24 }}
       />
+      <View style={{ padding: 24, paddingBottom: 32 }}>
+        <Button title="Nova meta" onPress={() => router.navigate("/target")} />
+      </View>
     </View>
   );
 }
