8b77911f407aab6aa5544e21f3540c96ad6927c6
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-17 22:34:36 -0300

    Unificando importação dos tokens de cores e fontes

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,12 +1,17 @@
-import { LinearGradient } from 'expo-linear-gradient'
-import { styles } from './styles'
-import { colors } from '@/theme/colors'
+import { LinearGradient } from "expo-linear-gradient";
+import { styles } from "./styles";
+import { colors } from "@/theme/colors";
+import { Text, View } from "react-native";
 
 export function HomeHeader() {
   return (
     <LinearGradient
       colors={[colors.blue[500], colors.blue[800]]}
       style={styles.container}
-    ></LinearGradient>
-  )
+    >
+      <View>
+        <Text style={styles.label}>Total que você possui</Text>
+      </View>
+    </LinearGradient>
+  );
 }
