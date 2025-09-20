f78de458729a57616bf1f6a0bee0b69c0ca9b300
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-20 14:12:03 -0300

    Conexão com o banco de dados

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,6 +7,9 @@ import {
   Inter_700Bold,
 } from "@expo-google-fonts/inter";
 import { Loading } from "@/components/Loading";
+import { SQLiteProvider } from "expo-sqlite";
+import { migrate } from "@/dataBase/migrate";
+import { Suspense } from "react";
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
@@ -20,11 +23,15 @@ export default function Layout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: colors.white },
-      }}
-    />
+    <Suspense fallback={<Loading />}>
+      <SQLiteProvider databaseName="target.db" onInit={migrate} useSuspense>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: colors.white },
+          }}
+        />
+      </SQLiteProvider>
+    </Suspense>
   );
 }
