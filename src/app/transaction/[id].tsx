63c9c7ffe9055beb6f6f9ac8d360f61483086b24
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-19 22:31:37 -0300

    Componente TransactionType

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,10 +1,14 @@
 import { CurrencyInput } from "@/components/CurrencyInput";
 import { Input } from "@/components/Input";
 import { PageHeader } from "@/components/PageHeader";
+import { TransactionType } from "@/components/TransactionType";
+import { TransactionTypes } from "@/utils/TransactionTypes";
 import { router, useLocalSearchParams } from "expo-router";
+import { useState } from "react";
 import { Button, Text, View } from "react-native";
 
 export default function Transaction() {
+  const [type, setType] = useState(TransactionTypes.Input);
   const params = useLocalSearchParams<{ id: string }>();
 
   return (
@@ -14,6 +18,7 @@ export default function Transaction() {
         subtitle="A cada valor guardado você fica mais próximo da sua meta. Se esforce para guardar e evitar retirar."
       />
       <View style={{ marginTop: 32, gap: 24 }}>
+        <TransactionType selected={type} onChange={setType} />
         <CurrencyInput label="Valor (R$)" value={0} />
 
         <Input
