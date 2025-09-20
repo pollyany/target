49618036ce201d23b6d1fa634499b2d1ed0f5fd5
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-20 18:22:25 -0300

    Listando transações da meta

--- src/dataBase/useTransactionsDatabase.ts
+++ src/dataBase/useTransactionsDatabase.ts
@@ -1,13 +1,22 @@
-import { useSQLiteContext } from 'expo-sqlite'
+import { useSQLiteContext } from "expo-sqlite";
 
 export type TransactionCreate = {
-  target_id: number
-  amount: number
-  observation?: string
-}
+  target_id: number;
+  amount: number;
+  observation?: string;
+};
+
+export type TransactionResponse = {
+  id: number;
+  target_id: number;
+  amount: number;
+  observation: string;
+  created_at: Date;
+  updated_at: Date;
+};
 
 export function useTransactionsDatabase() {
-  const database = useSQLiteContext()
+  const database = useSQLiteContext();
 
   async function create(data: TransactionCreate) {
     const statement = await database.prepareAsync(`
@@ -15,14 +24,23 @@ export function useTransactionsDatabase() {
           (target_id, amount, observation)
         VALUES
           ($target_id, $amount, $observation)
-      `)
+      `);
 
     statement.executeAsync({
       $target_id: data.target_id,
       $amount: data.amount,
       $observation: data.observation,
-    })
+    });
   }
 
-  return { create }
+  function listByTargetId(id: number) {
+    return database.getAllAsync<TransactionResponse>(`
+        SELECT id, target_id, amount, observation, created_at, updated_at
+        FROM transactions
+        WHERE target_id = ${id}
+        ORDER BY created_at DESC
+      `);
+  }
+
+  return { create, listByTargetId };
 }
