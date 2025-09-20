498a117f11572cff1e8355e4b5e94e4e1f4f5478
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-20 14:08:55 -0300

    Criando a tabela Transactions

--- src/dataBase/migrate.ts
+++ src/dataBase/migrate.ts
@@ -1,7 +1,7 @@
-import {type SQLiteDatabase } from "expo-sqlite";
+import { type SQLiteDatabase } from "expo-sqlite";
 
 export async function migrate(dataBase: SQLiteDatabase) {
-    await dataBase.execAsync(`
+  await dataBase.execAsync(`
     PRAGMA foreign_keys = ON;
 
     CREATE TABLE IF NOT EXISTS targets (
@@ -10,6 +10,18 @@ export async function migrate(dataBase: SQLiteDatabase) {
         amount FLOAT NOT NULL,
         created_at timestamp NOT NULL DEFAULT current_timestamp,
         updated_at timestamp NOT NULL DEFAULT current_timestamp
+    );
+
+    CREATE TABLE IF NOT EXISTS transactions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        target_id INTEGER NOT NULL,
+        amount FLOAT NOT NULL,
+        observation TEXT NULL,
+        created_at timestamp NOT NULL DEFAULT current_timestamp,
+        updated_at timestamp NOT NULL DEFAULT current_timestamp,
+        CONSTRAINT fk_targets_transactions
+        FOREIGN KEY (target_id) REFERENCES targets(id)
+        ON DELETE CASCADE
     );
   `);
 }
