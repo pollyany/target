a339da42c94700add861642717d3d482576e470e
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-09-20 17:44:34 -0300

    Removendo do banco

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,93 +1,121 @@
-import { Button } from "@/components/Button";
-import { CurrencyInput } from "@/components/CurrencyInput";
-import { Input } from "@/components/Input";
-import { PageHeader } from "@/components/PageHeader";
-import { useTargetDatabase } from "@/dataBase/useTargetDatabase";
-import { router, useLocalSearchParams } from "expo-router";
-import { useEffect, useState } from "react";
-import { Alert, View } from "react-native";
+import { Button } from '@/components/Button'
+import { CurrencyInput } from '@/components/CurrencyInput'
+import { Input } from '@/components/Input'
+import { PageHeader } from '@/components/PageHeader'
+import { useTargetDatabase } from '@/dataBase/useTargetDatabase'
+import { router, useLocalSearchParams } from 'expo-router'
+import { useEffect, useState } from 'react'
+import { Alert, View } from 'react-native'
 
 export default function Target() {
-  const [isProcessing, setIsProcessing] = useState(false);
-  const [name, setName] = useState("");
-  const [amount, setAmount] = useState(0);
+  const [isProcessing, setIsProcessing] = useState(false)
+  const [name, setName] = useState('')
+  const [amount, setAmount] = useState(0)
 
-  const params = useLocalSearchParams<{ id?: string }>();
-  const targetDatabase = useTargetDatabase();
+  const params = useLocalSearchParams<{ id?: string }>()
+  const targetDatabase = useTargetDatabase()
 
   function handleSave() {
     if (!name.trim() || amount <= 0) {
       return Alert.alert(
-        "Atenção",
-        "Preencha o nome e o valor precisa ser maior que zero."
-      );
+        'Atenção',
+        'Preencha o nome e o valor precisa ser maior que zero.',
+      )
     }
 
-    setIsProcessing(true);
+    setIsProcessing(true)
 
     if (params.id) {
-      // update
       update()
     } else {
-      create();
+      create()
     }
   }
 
   async function update() {
     try {
-      await targetDatabase.update({ id: Number(params.id), name, amount });
-      Alert.alert("Sucesso!", "Meta atualizada com sucesso!", [
+      await targetDatabase.update({ id: Number(params.id), name, amount })
+      Alert.alert('Sucesso!', 'Meta atualizada com sucesso!', [
         {
-          text: "Ok",
+          text: 'Ok',
           onPress: router.back,
         },
-      ]);
+      ])
     } catch (error) {
-      Alert.alert("Erro", "Não foi possível atualizar a meta.");
-      console.log(error);
-      setIsProcessing(false);
+      Alert.alert('Erro', 'Não foi possível atualizar a meta.')
+      console.log(error)
+      setIsProcessing(false)
     }
   }
 
   async function create() {
     try {
-      await targetDatabase.create({ name, amount });
-      Alert.alert("Nova Meta", "Meta criada com sucesso!", [
+      await targetDatabase.create({ name, amount })
+      Alert.alert('Nova Meta', 'Meta criada com sucesso!', [
         {
-          text: "Ok",
+          text: 'Ok',
           onPress: router.back,
         },
-      ]);
+      ])
     } catch (error) {
-      Alert.alert("Erro", "Não foi possível criar a meta.");
-      console.log(error);
-      setIsProcessing(false);
+      Alert.alert('Erro', 'Não foi possível criar a meta.')
+      console.log(error)
+      setIsProcessing(false)
     }
   }
 
   async function fetchDetails(id: number) {
     try {
-      const response = await targetDatabase.show(id);
-      setName(response.name);
-      setAmount(response.amount);
+      const response = await targetDatabase.show(id)
+      setName(response.name)
+      setAmount(response.amount)
     } catch (error) {
-      Alert.alert("Erro", "Não foi possível carregar os detalhes da meta.");
-      console.log(error);
+      Alert.alert('Erro', 'Não foi possível carregar os detalhes da meta.')
+      console.log(error)
+    }
+  }
+
+  function handleRemove() {
+    if (!params.id) {
+      return
+    }
+
+    Alert.alert('Remover', 'Deseja realemente remover?', [
+      { text: 'Não', style: 'cancel' },
+      { text: 'Sim', onPress: remove },
+    ])
+  }
+
+  async function remove() {
+    try {
+      setIsProcessing(true)
+
+      await targetDatabase.remove(Number(params.id))
+      Alert.alert('Meta', 'Meta removida!', [
+        { text: 'Ok', onPress: () => router.replace('/') },
+      ])
+    } catch (error) {
+      Alert.alert('Erro', 'Não foi possível remover a meta.')
+      console.log(error)
     }
   }
 
   useEffect(() => {
     if (params.id) {
-      fetchDetails(Number(params.id));
+      fetchDetails(Number(params.id))
     }
-  }, [params.id]);
+  }, [params.id])
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader
         title="Meta"
         subtitle="Economize para alcançar sua meta financeira."
+        rightButton={
+          params.id ? { icon: 'delete', onPress: handleRemove } : undefined
+        }
       />
+
       <View style={{ marginTop: 32, gap: 24 }}>
         <Input
           label="Nova meta"
@@ -95,11 +123,13 @@ export default function Target() {
           onChangeText={setName}
           value={name}
         />
+
         <CurrencyInput
           label="Valor alvo (R$)"
           value={amount}
           onChangeValue={setAmount}
         />
+
         <Button
           title="Salvar"
           isProcessing={isProcessing}
@@ -107,5 +137,5 @@ export default function Target() {
         />
       </View>
     </View>
-  );
+  )
 }
